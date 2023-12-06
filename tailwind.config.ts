86e800303c33b28fa5281ae4a0d7042985e7c525
xosnrdev <successxodev@gmail.com>
2023-12-06 09:34:43 -0500

    build: tailwindcss breakpoints configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,11 @@ module.exports = {
       center: true,
       padding: '2rem',
       screens: {
-        '2xl': '1400px',
+        xs: '375px',
+        sm: '600px',
+        md: '900px',
+        lg: '1200px',
+        xl: '1536px',
       },
     },
     extend: {
