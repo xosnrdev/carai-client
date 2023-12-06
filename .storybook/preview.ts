4b2440bedc0b7e4bf336d1b1b4afe264d972282e
xosnrdev <successxodev@gmail.com>
2023-12-06 08:57:50 -0500

    build: storybook breakpoint configuration

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,16 @@
 import type { Preview } from '@storybook/react';
 
+const BREAKPOINTS_INT = { xs: 375, sm: 600, md: 900, lg: 1200, xl: 1536 };
+
+const customViewports = {};
+
+for (const [key, val] of Object.entries(BREAKPOINTS_INT)) {
+  customViewports[key] = {
+    name: key,
+    styles: { width: `${val}px`, height: `${(key.charCodeAt(0) - 96) * 10}vh` },
+  };
+}
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -9,6 +20,7 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    viewport: { viewports: customViewports },
   },
 };
 
