1fafea832c82e45796551a9de0e23a8d211c2251
xosnrdev <successxodev@gmail.com>
2023-12-06 19:23:13 -0500

    build: prettier tailwindcss lint

--- components/ui/header/header.tsx
+++ components/ui/header/header.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from 'react';
-import Brand from '../brand/brand';
+import { FC } from 'react';
 import ThemeSwitch from '../../utils/ThemeSwitch';
+import Brand from '../brand/brand';
 
 const Header: FC = () => {
   return (
     <header className="max-width__wrapper w-full bg-secondary lg:py-3 xl:py-3">
-      <div className="flex flex-row justify-between items-center">
+      <div className="flex flex-row items-center justify-between ">
         <Brand />
         <ThemeSwitch />
       </div>
