e842f462f7bbe9b2cc90160d61a916b4b72ec49c
xosnrdev <successxodev@gmail.com>
2023-12-07 13:05:48 -0500

    refactor: layout structure and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Header from '@/components/ui/header/header';
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <Header />
+      <h1 className="text-4xl text-primary">
+        Under construction at the moment
+      </h1>
     </main>
   );
 }
